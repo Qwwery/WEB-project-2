9dcb75f116b81cfede124f790aec77bda704f5bb
user <regeneration76@yandex.ru>
2024-03-08 14:35:11 +0300

    фикс

--- main.py
+++ main.py
@@ -13,45 +13,42 @@ def create_user(surname, name, age, position, speciality, address, email, hashed
                 modified_date=None):
     db_sess = db_session.create_session()
     user = User()
+    user.surname = surname
+    user.name = name
+    user.age = age
+    user.position = position
+    user.speciality = speciality
+    user.address = address
+    user.email = email
+    user.hashed_password = hashed_password
+    if modified_date:
+        user.modified_date = modified_date
     try:
-        user.surname = surname
-        user.name = name
-        user.age = age
-        user.position = position
-        user.speciality = speciality
-        user.address = address
-        user.email = email
-        user.hashed_password = hashed_password
-        if modified_date:
-            user.modified_date = modified_date
+        db_sess.add(user)
+        db_sess.commit()
+        print('ok')
     except Exception:
         raise Exception('Вы передали не все параметры!!!')
-    db_sess.add(user)
-    db_sess.commit()
-    print('ok')
-
 
 def add_job(team_leader, job, work_size, collaborators, start_date=None, end_date=None, is_finished=None):
     db_sess = db_session.create_session()
     jobs = Jobs()
 
+    jobs.team_leader = team_leader
+    jobs.job = job
+    jobs.work_size = work_size
+    jobs.collaborators = collaborators
+    if start_date:
+        jobs.start_date = start_date
+    if end_date:
+        jobs.end_date = end_date
+    if is_finished:
+        jobs.is_finished = is_finished
     try:
-        jobs.team_leader = team_leader
-        jobs.job = job
-        jobs.work_size = work_size
-        jobs.collaborators = collaborators
-        if start_date:
-            jobs.start_date = start_date
-        if end_date:
-            jobs.end_date = end_date
-        if is_finished:
-            jobs.is_finished = is_finished
+        db_sess.add(jobs)
+        db_sess.commit()
     except Exception:
         raise Exception('Вы передали не все обязательные параметры!!!')
-    db_sess.add(jobs)
-    db_sess.commit()
-
-
 def main():
     db_session.global_init("db/mars_explorer.db")
     create_user('Scott', 'Ridley', 21, 'captain', 'research engineer', 'module_1', 'scott_chief@mars.org', '1111')
