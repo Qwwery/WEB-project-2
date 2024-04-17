903358852ddd7b1f0ff83d79474a4c32b03215de
Valera <valerylarionov06@gmail.com>
2024-04-18 00:10:43 +0300

    замена адреса тест

--- api.py
+++ api.py
@@ -1,9 +1,18 @@
-import requests
+import socket
 
 
+# Получаем локальный IPv4 адрес
 def get_ip():
     try:
-        return requests.get('https://api.ipify.org/?format=json%27').text
-    except Exception as e:
-        print(e)
-        return 'не удалось пробить'
+        local_ip = socket.gethostbyname(socket.gethostname())
+        return local_ip
+    except socket.gaierror as e:
+        try:
+            # Получаем локальный IPv6 адрес
+            local_ip = socket.gethostbyname(socket.gethostname())
+            return local_ip
+        except socket.gaierror as e:
+            return 'Хорошо шифруется'
+
+
+get_ip()
